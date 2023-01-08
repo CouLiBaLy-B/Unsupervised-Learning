f2a1f9d5cd86e98ccf725ce5b52e435b20c610ee
COULIBALY Bourahima <91073443+CouLiBaLy-B@users.noreply.github.com>
2023-01-08 22:42:20 +0100

    Update unsupervised.py

--- unsupervised.py
+++ unsupervised.py
@@ -228,6 +228,8 @@ for i in range(B.shape[0]):
         D[i][np.sum(range(B.shape[1]))- np.sum(range(B.shape[1]-j))+k] = 2*B[i][j]* B[i][k]
       else :
         D[i][np.sum(range(B.shape[1]))- np.sum(range(B.shape[1]-j))+k] = B[i][j]* B[i][k]
+        
+ smb_d = D.copy()
 'La nouvelle matrice des probabilités est : ',D
 'Vérification du caractère stochastique de la matrice', D.sum(axis = 1)
 
